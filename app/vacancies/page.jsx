760ce986b1c829d14fe0efe9630b9c9f51b62a0b
fatimamammadova <fatimammmadova@gmail.com>
2024-02-17 23:50:36 +0400

    vacancies

---
+++ app/vacancies/page.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import "./_vacancies.scss";
+
+export const generateMetadata = () => {
+  return {
+    title: "Vakansiyalar",
+  };
+};
+
+export const Vacancies = () => {
+  return (
+    <main>
+      <section id="vacancies">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 wrapping">
+              <div className="page-title">
+                <p>Vakansiyalar</p>
+              </div>
+              <div className="about-content">
+                <p>
+                Hal-hazırda aktiv vakansiya yoxdur
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Vacancies;
